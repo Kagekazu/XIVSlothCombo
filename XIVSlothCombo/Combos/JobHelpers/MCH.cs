277269a91fb0d7cec052e7980de761e8fdb53e9e
Kage <wijnsmattia@gmail.com>
2024-06-28 21:47:01 +0200

    API X fix

--- XIVSlothCombo/Combos/JobHelpers/MCH.cs
+++ XIVSlothCombo/Combos/JobHelpers/MCH.cs
@@ -1,6 +1,8 @@
-﻿using ECommons.DalamudServices;
+﻿using Dalamud.Game.ClientState.JobGauge.Types;
+using ECommons.DalamudServices;
 using System.Linq;
 using XIVSlothCombo.Combos.JobHelpers.Enums;
+using XIVSlothCombo.Combos.PvE;
 using XIVSlothCombo.CustomComboNS.Functions;
 using XIVSlothCombo.Data;
 
@@ -75,9 +77,6 @@ namespace XIVSlothCombo.Combos.JobHelpers
             DoubleCheck,
             HeatedSlugShot,
             HeatedCleanShot];
-
-
-
 
         public static bool LevelChecked => CustomComboFunctions.LocalPlayer.Level >= OpenerLevel;
 
